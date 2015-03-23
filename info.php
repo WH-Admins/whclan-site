732bde58fb17ec88032511d15857eef4414785f4
Label <Label@users.noreply.github.com>
2015-03-23 23:51:02 +0100

    Update info.php
    added one news item

--- info.php
+++ info.php
@@ -20,6 +20,7 @@
     <div style="margin-top:20">
       <div class="splitleft"><div class="whitebox">
   	<em>Team News</em>
+  	    <div class="newsitem"> Label and Evel organised a scrim vs Implosion &lt;i&gt;! This sunday March 29th at 8 pm GMT right after SL. </div>
         <div class="newsitem"> LewisH brings our very own clan server to life! Scrims in the future will be a lot less painful to organise. <a href="http://www.reddit.com/r/whclan/comments/2zxign/clan_server/">22/03/15 19:18 GMT</a> </div>
         <div class="newsitem"> Our subreddit is now online! <a href="http://www.reddit.com/r/whclan/comments/2zu98r/label_here/">21/03/15 21:14 GMT</a> </div>
       </div></div>
