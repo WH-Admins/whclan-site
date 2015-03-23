a92c19679798acd66f88c605672bc8a92e3e3f9c
Chris Gadzinski <zenmags@gmail.com>
2015-03-23 19:55:49 +0000

    added url to google docs

--- restricted.php
+++ restricted.php
@@ -20,7 +20,7 @@
 
     <p> This is a test restricted page. </p>
     <p> Only logged in members should be able to view this. </p>
-    <p> Gj Breeze, you're in </p>
+    <p> Gj Breeze, you're in. Here's the link: <a href="https://docs.google.com/spreadsheets/d/1SzqZvYW-npuz9vYqTB6ovU4QYf0fgXcWkhZz4bJvJZ4/edit#gid=1529951854">google docs</a> </p>
 
 
 <?php
