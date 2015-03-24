a4aa62f0ddf0060d7d80be21ad8b0f77bcaffe5a
Magnetic Duck <zenmags@gmail.com>
2015-03-24 15:11:06 +0000

    added to-do list, rewrote index page a bit

--- default.php
+++ default.php
@@ -10,16 +10,27 @@
   <div class="container">
     <h2>This is the central hub for all of WH's action.</h2>
     <p> WH members, put this site on your favourites, make it your homepage, print it out and tape it to your forehead, or just refresh it every 
-once and a while; whatever makes more sense to you. <b>If you want to know what's going on</b>, you should be able to find the answer here. Me 
-(MagneticDuck) and the other cool developers on the IRC channel will add lots of features to it, all with this primary objective: to be 
-the go-to location for WH members, providing the 3 factors necessary for us to function as an effective part of the team: </p> 
-    <h3> information, organisation, and communication. </h3>
+once and a while; whatever makes more sense to you. <b>If you want to know what's going on</b>, you should be able to find the answer here. </p>
+    This site is the central element of the ongoing initiative to centralise and organise the WH clan; our goal is to make clan-wide communication easy, catalise internal discussion in realtime and on Reddit, and finally to bring a more professional feel. These three goals can be summerized in the following three values:
+    <h3> communication, information, and organisation. </h3>
     <div class="brightbox">
       <p class="important"> WH is dedicated to improving ourselves as players and as a competitive team. However, we also intend to play a role as a social agent, bringing progress and positivity to altitude through the organised effort of a playerbase that is passoinate about the game. </p>
     </div>
     <div>
       <div class="splitleft"><div class="greybox">
-        <a href="http://bringvictory.com"><em>Here</em></a> is our roster on Google Docs. Available to the public so anybody can edit! Thanks to all the people who helped me fill this out. It's still not complete, but we're getting good information on lots of stuff. If you have ideas for team composition, tell us on reddit or IRC; if you have factual information to add, edit it directly into the roster.</li>
+        <p><h3>TO-DO list</h3></p>
+        <p> Lots of things have been happening! Here are some things that have yet to been concluded:
+        <p><em>for developer</em></p>
+        <ul>
+          <li>Make reddit scheme cleaner and more suggestive of altitude, and add a way to tag posts according to their content. (Assigned to MagneticDuck)</li> 
+          <li>Investigate the addition of some sort of mailing list.</li> 
+          <li>Investigate the addition of an automated system to put adequately classified reddit posts on the site's news feed.</li> 
+        </ul>
+        <p><em>for clan members</em></p>
+        <ul>
+          <li>Populate our <a href="http://www.reddit.com/r/whclan">subreddit</a></li>
+          <li>Populate our IRC channel (make a post in the Roll Call)</li>
+        </ul>
       </div> </div>
       <div class="splitright"><div class="nobox">
         <div align="center"> <img src="http://i.imgur.com/XYDxm10.jpg" width=500/> </div>
