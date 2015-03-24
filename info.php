ef6aab4ee68c89f74a4434f0a6b945d24b9f331e
Magnetic Duck <zenmags@gmail.com>
2015-03-24 15:13:29 +0000

    paragraph spacing in info.php

--- info.php
+++ info.php
@@ -19,13 +19,13 @@
 
     <div style="margin-top:20">
       <div class="splitleft"><div class="whitebox">
-  	<em>Team News</em>
+  	<p><em>Team News</em></p>
   	    <div class="newsitem"> Label and Evel organised a scrim vs Implosion &lt;i&gt;! This sunday March 29th at 8 pm GMT right after SL. </div>
         <div class="newsitem"> LewisH brings our very own clan server to life! Scrims in the future will be a lot less painful to organise. <a href="http://www.reddit.com/r/whclan/comments/2zxign/clan_server/">22/03/15 19:18 GMT</a> </div>
         <div class="newsitem"> Our subreddit is now online! <a href="http://www.reddit.com/r/whclan/comments/2zu98r/label_here/">21/03/15 21:14 GMT</a> </div>
       </div></div>
       <div class="splitleft"><div class="greybox">
-  	<em>Team Calandar</em>
+  	<p><em>Team Calandar</em></p>
         <div> scrim vs implosion &lt;i&gt;<br>
         sunday March, 29th @ 8 pm GMT</div>
       </div></div>
