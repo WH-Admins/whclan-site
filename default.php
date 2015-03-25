07897b5579e88021f2c2825060f877be9f383e3e
Magnetic Duck <zenmags@gmail.com>
2015-03-25 00:19:26 +0000

    working

--- default.php
+++ default.php
@@ -15,24 +15,26 @@
 once and a while; whatever makes more sense to you. <b>If you want to know what's going on</b>, you should be able to find the answer here. </p>
     This site is the central element of the ongoing initiative to centralise and organise the WH clan; our goal is to make clan-wide communication easy, catalise internal discussion in realtime and on Reddit, and finally to bring a more professional feel. These three goals can be summerized in the following three values:
     <h3> communication, information, and organisation. </h3>
-    <div class="brightbox">
+    <div class="paddedbox">
       <p class="important"> WH is dedicated to improving ourselves as players and as a competitive team. However, we also intend to play a role as a social agent, bringing progress and positivity to altitude through the organised effort of a playerbase that is passoinate about the game. </p>
     </div>
     <div>
-      <div class="split"><div class="greybox">
-        <p><h3>TO-DO list</h3></p>
-        <p> Lots of things have been happening! Here are some things that have yet to been concluded:
-        <p><em>for developers</em></p>
-        <ul>
-          <li><strike>Make reddit scheme cleaner and more suggestive of altitude, and add a way to tag posts according to their content. (Assigned to MagneticDuck)</strike> done <8) </li>
-          <li>Investigate the addition of some sort of mailing list.</li>
-          <li><strike>Investigate the addition of an automated system to put adequately classified reddit posts on the site's news feed.</strike> done ^_^</li>
-        </ul>
-        <p><em>for clan members</em></p>
-        <ul>
-          <li>Populate our <a href="http://www.reddit.com/r/whclan">subreddit</a>(make a post in the Roll Call)</li>
-          <li>Populate our IRC channel </li>
-        </ul>
+      <div class="split"><div class="paddedbox">
+        <div class="titlebox">TO-DO list</div>
+        <div class="contentbox">
+          <p> Lots of things have been happening! Here are some things that have yet to been concluded:
+          <p><em>for developers</em></p>
+          <ul>
+            <li><strike>Make reddit scheme cleaner and more suggestive of altitude, and add a way to tag posts according to their content. (Assigned to MagneticDuck)</strike> done <8) </li>
+            <li>Investigate the addition of some sort of mailing list.</li>
+            <li><strike>Investigate the addition of an automated system to put adequately classified reddit posts on the site's news feed.</strike> done ^_^</li>
+          </ul>
+          <p><em>for clan members</em></p>
+          <ul>
+            <li>Populate our <a href="http://www.reddit.com/r/whclan">subreddit</a>(make a post in the Roll Call)</li>
+            <li>Populate our IRC channel </li>
+          </ul>
+        </div> <!-- content box ends -->
       </div> </div>
       <div class="split"><div class="nobox">
         <div align="center"> <img style="margin-top: 10px; width: 100%;" src="http://i.imgur.com/XYDxm10.jpg" width=500/> </div>
