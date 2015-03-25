2100a87b8bad8f98321563370ed2954a1c6c8d15
Magnetic Duck <zenmags@gmail.com>
2015-03-25 00:27:56 +0000

    working on comms

--- comms.php
+++ comms.php
@@ -16,13 +16,21 @@
     </div>
 
     <div class="paddedbox">
-      <div class="split"><div class="greybox">
-        <a href="http://www.reddit.com/r/whclan"><em>Reddit</em></a>
-        <p> This is where we talk about stuff over longer periods of time. Want everybody's opinion on the role of acid loopy in TBD? Make a post here. </p>
+      <div class="split"><div class="paddedbox">
+        <div class="titlebox">
+          <a href="http://www.reddit.com/r/whclan"><em>Reddit</em></a>
+        </div>
+        <div class="contentbox">
+          <p> This is where we talk about stuff over longer periods of time. Want everybody's opinion on the role of acid loopy in TBD? Make a post here. </p>
+        </div>
       </div></div>
-      <div class="split"><div class="whitebox">
-        <a href="http://webchat.freenode.net/"><em>IRC</em></a> (Internet Relay Chat)
-        <p> Our real-time chatroom! Put your altitude nick in the Nickname field and #wreaking under Channels, and join the discussion; there are already a lot of clan members hanging around here. (Desktop and mobile clients also available; we recommend hexchat for windows, XChat Azure for mac, AndChat for android, and weechat-cli for Linux.) <b>The best way</b> to use this is to <b>stay online</b> as long as you're near the computer; conversations usually pick up spontaneously. We've already had some good ones. We will also be using this for pre- and post- debriefings on scrim / practice days. If I'm anywhere near a computer, this is where you can contact me. </p>
+      <div class="split"><div class="paddedbox">
+        <div class="titlebox">
+          <a href="http://webchat.freenode.net/"><em>IRC</em></a> (Internet Relay Chat)
+        </div>
+        <div class="contentbox">
+          <p> Our real-time chatroom! Put your altitude nick in the Nickname field and #wreaking under Channels, and join the discussion; there are already a lot of clan members hanging around here. (Desktop and mobile clients also available; we recommend hexchat for windows, XChat Azure for mac, AndChat for android, and weechat-cli for Linux.) <b>The best way</b> to use this is to <b>stay online</b> as long as you're near the computer; conversations usually pick up spontaneously. We've already had some good ones. We will also be using this for pre- and post- debriefings on scrim / practice days. If I'm anywhere near a computer, this is where you can contact me. </p>
+        </div>
       </div></div>
     </div>
 
