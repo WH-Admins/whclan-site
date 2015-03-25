5fe3417c3da9f243c69429a8e7770f1ab9272eb2
Magnetic Duck <zenmags@gmail.com>
2015-03-25 00:37:34 +0000

    ughhh

--- default.php
+++ default.php
@@ -27,9 +27,7 @@
           <p> Lots of things have been happening! Here are some things that have yet to been concluded:
           <p><em>for developers</em></p>
           <ul>
-            <li><strike>Make reddit scheme cleaner and more suggestive of altitude, and add a way to tag posts according to their content. (Assigned to MagneticDuck)</strike> done <8) </li>
             <li>Investigate the addition of some sort of mailing list.</li>
-            <li><strike>Investigate the addition of an automated system to put adequately classified reddit posts on the site's news feed.</strike> done ^_^</li>
           </ul>
           <p><em>for clan members</em></p>
           <ul>
@@ -37,7 +35,6 @@
             <li>Populate our IRC channel </li>
           </ul>
         </div> <!-- content box ends -->
->>>>>>> devel
       </div> </div>
       <div class="split"><div class="nobox">
         <div align="center"> <img style="width: 100%;" src="http://i.imgur.com/XYDxm10.jpg" width=500/> </div>
