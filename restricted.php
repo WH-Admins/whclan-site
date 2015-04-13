c7bed2da2e2f927ed5cf670669a1c17d0254f389
Magnetic Duck <zenmags@gmail.com>
2015-04-13 14:53:32 +0200

    organising restricted page

--- restricted.php
+++ restricted.php
@@ -7,26 +7,33 @@
 <body>
   <style> #restricted-link { font-weight:bold; } </style>
   <?php include "header.php"; ?>
-  <div class="container">
-    <h2>Test Page</h2>
-
+    <div class="headbox">
+      <div class="headimg" style="background-image:url(images/head-whale-left.png)"> </div>
+      <div class="headtext"> Restricted member-only resources. </div>
+      <div class="headimg" style="background-image:url(images/head-whale-right.png)"> </div>
+    </div>
+  <div class="container headmargin">
+    <div class="paddedbox"> 
 <?php
     if ($login->isUserLoggedIn() == false) {
 ?>
+
       <p style="color: #F66;"> Only members can view this page, click <a href='login.php'>here</a> to login.</p>
+
 <?php
     } else {
 ?>
-
-    <p> This is a test restricted page. </p>
-    <p> Only logged in members should be able to view this. </p>
-    <p> Gj Breeze, you're in. Here's the link: <a href="https://docs.google.com/spreadsheets/d/1SzqZvYW-npuz9vYqTB6ovU4QYf0fgXcWkhZz4bJvJZ4/edit#gid=1529951854">google docs</a> </p>
-
-
+    <p class="important"> Some stuff is best kept secret. </p>
+    <div class="titlebox"> Some secret stuff </div>
+    <div class="contentbox"> 
+      <p> link to the Google Docs: <a href="https://docs.google.com/spreadsheets/d/1SzqZvYW-npuz9vYqTB6ovU4QYf0fgXcWkhZz4bJvJZ4/edit#gid=1529951854">here</a> </p>
+    </div>
 <?php
     }
 ?>
 
+    </div>
+  </div>
   </div>
 </body>
 </html>
