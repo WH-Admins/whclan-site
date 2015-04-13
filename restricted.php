687d15f546bed58789152e3d40b61dfc3456234e
Magnetic Duck <zenmags@gmail.com>
2015-04-13 14:55:24 +0200

    fixing things

--- restricted.php
+++ restricted.php
@@ -9,7 +9,7 @@
   <?php include "header.php"; ?>
     <div class="headbox">
       <div class="headimg" style="background-image:url(images/head-whale-left.png)"> </div>
-      <div class="headtext"> Restricted member-only resources. </div>
+      <h2 class="headtext"> Restricted member-only resources. </h2>
       <div class="headimg" style="background-image:url(images/head-whale-right.png)"> </div>
     </div>
   <div class="container headmargin">
@@ -24,7 +24,7 @@
     } else {
 ?>
     <p class="important"> Some stuff is best kept secret. </p>
-    <div class="titlebox"> Some secret stuff </div>
+    <div class="titlebox headless"> Some secret stuff </div>
     <div class="contentbox"> 
       <p> link to the Google Docs: <a href="https://docs.google.com/spreadsheets/d/1SzqZvYW-npuz9vYqTB6ovU4QYf0fgXcWkhZz4bJvJZ4/edit#gid=1529951854">here</a> </p>
     </div>
