a101aaaf7a20a77474cc995bb5b04c3cc73af3c5
Magnetic Duck <zenmags@gmail.com>
2015-04-20 15:11:52 +0200

    cough

--- comms.php
+++ comms.php
@@ -29,10 +29,12 @@
       </div></div>
       <div class="split"><div class="paddedbox nohead">
         <div class="titlebox">
-IRC: <a href="https://kiwiirc.com/client/irc.freenode.net/?nick=Nickname|?#wreaking"> <img url="https://kiwiirc.com/buttons/irc.freenode.net/wreaking.png"> </a>
+          IRC (Internet Relay Chat)
         </div>
         <div class="contentbox">
-          <p> Our real-time chatroom! Put your altitude nick in the Nickname field and #wreaking under Channels, and join the discussion; there are already a lot of clan members hanging around here. (Desktop and mobile clients also available; we recommend hexchat for windows, XChat Azure for mac, AndChat for android, and weechat-cli for Linux.) <b>The best way</b> to use this is to <b>stay online</b> as long as you're near the computer; conversations usually pick up spontaneously. We've already had some good ones. We will also be using this for pre- and post- debriefings on scrim / practice days. If I'm anywhere near a computer, this is where you can contact me. </p>
+          <p> Our real-time chatroom! Click the link at the bottom of this paragraph and join the discussion! There are already a lot of clan members hanging around here. <b>The best way</b> to use this is to <b>stay online</b> as long as you're near the computer; conversations can pick up spontaneously. We will also be using this for pre- and post- debriefings on scrim / practice days and clan meetings. If I'm anywhere near a computer, this is where you can contact me. </p>
+         <p> Link to a handy web client: <a href="https://kiwiirc.com/client/irc.freenode.net/?nick=Nickname|?#wreaking"> <img src="https://kiwiirc.com/buttons/irc.freenode.net/wreaking.png"> </a> </p>
+         <p> If you already use IRC: point your client to #wreaking on irc.freenode.net </p>
         </div>
       </div></div>
     </div>
