21d20ca21ad5e3ad69a1ee1f2283cb2c982feeb8
Lewis <lewis@haii.uk>
2015-04-20 18:12:01 +0100

    added APL1 videos

--- videos.php
+++ videos.php
@@ -17,6 +17,35 @@
   <div class="container headmargin">
   <div class="paddedbox">
 
+<h3> April 19th, APL Week 1 </h3>
+<div class="thumbgroup">
+<div class="videothumb"><a href="https://youtu.be/GdLxdeIslTo">
+<img src="thumbs/190415_rotw_qq_scrapyard.png"></a>
+<div class="thumbtext"> &lt8) RoTW vs qq - <b>Scrapyard</b> </div>
+</div>
+<div class="videothumb"><a href="https://youtu.be/y8vQoDcaAtc">
+<img src="thumbs/190415_rotw_qq_justice.png"></a>
+<div class="thumbtext"> &lt8) RoTW vs qq - <b>Justice</b> </div>
+</div>
+
+<div class="videothumb"><a href="https://youtu.be/bOtnMlbzP98">
+<img src="thumbs/190415_eu_i_cave.png"></a>
+<div class="thumbtext"> &lt8) EU vs &lti&gt- <b>Cave</b> </div>
+</div>
+<div class="videothumb"><a href="https://youtu.be/MdOcRxDc_VQ">
+<img src="thumbs/190415_eu_i_focus.png"></a>
+<div class="thumbtext"> &lt8) EU vs &lti&gt - <b>Focus</b> </div>
+</div>
+
+<div class="videothumb"><a href="https://youtu.be/is2MyPVBMAI">
+<img src="thumbs/190415_eu_qq_asteroids.png"></a>
+<div class="thumbtext"> &lt8) EU vs qq - <b>Asteroids</b> </div>
+</div>
+<div class="videothumb"><a href="https://youtu.be/iqy4-vDVZ_k">
+<img src="thumbs/190415_eu_qq_core.png"></a>
+<div class="thumbtext"> &lt8) EU vs qq - <b>Core</b> </div>
+</div>
+
 <h3> March 24th, Scrim vs Implosion </h3>
 <div class="thumbgroup">
 <div class="videothumb"><a href="https://youtu.be/Jt15OAAyywE">
