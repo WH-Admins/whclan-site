86c1f15a9ae7d0fe3917e11a26cd925e7991ef62
Magnetic Duck <zenmags@gmail.com>
2015-04-20 15:15:32 +0200

    fixed things, added instructions

--- comms.php
+++ comms.php
@@ -32,8 +32,8 @@
           IRC (Internet Relay Chat)
         </div>
         <div class="contentbox">
-          <p> Our real-time chatroom! Click the link at the bottom of this paragraph and join the discussion! There are already a lot of clan members hanging around here. <b>The best way</b> to use this is to <b>stay online</b> as long as you're near the computer; conversations can pick up spontaneously. We will also be using this for pre- and post- debriefings on scrim / practice days and clan meetings. If I'm anywhere near a computer, this is where you can contact me. </p>
-         <p> Link to a handy web client: <a href="https://kiwiirc.com/client/irc.freenode.net/?nick=Nickname|?#wreaking"> <img src="https://kiwiirc.com/buttons/irc.freenode.net/wreaking.png"> </a> </p>
+          <p> Our real-time chatroom! Click the link at the bottom of this paragraph, set the 'Nickname' to something we can recognize and join the discussion! (If it says it's "already taken", add an underscore and try again. Repeat until success!) There are already a lot of clan members hanging around here. <b>The best way</b> to use this is to <b>stay online</b> as long as you're near the computer; conversations can pick up spontaneously. We will also be using this for pre- and post- debriefings on scrim / practice days and clan meetings. If I'm anywhere near a computer, this is where you can contact me. </p>
+         <p> Link to a handy web client: <a href="https://kiwiirc.com/client/irc.freenode.net/?nick=Nickname|?#wreaking"> <img src="https://kiwiirc.com/buttons/irc.freenode.net/wreaking.png" style="margin-bottom:-0.5em"> </a> </p>
          <p> If you already use IRC: point your client to #wreaking on irc.freenode.net </p>
         </div>
       </div></div>
