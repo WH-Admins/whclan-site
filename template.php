af15abb04c92d03187b56fd717ed2a0515aea17a
Magnetic Duck <zenmags@gmail.com>
2015-04-26 11:07:03 +0000

    added template

---
+++ template.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+  <?php include "imports.php"; ?>
+  <title>Template Page>
+</head>
+<body>
+  <?php include "header.php"; ?>
+  <div id="sitebanner">
+    <div class="headimg" style="background-image:url(images/head-randa-left.png)"> </div>
+    <h1 id="headtext"> #headtext </h1>
+    <div class="headimg" style="background-image:url(images/head-randa-left.png)"> </div>
+  </div>
+  <div id="contentarea">
+    <div class="stackbox">
+      <p> Here's some text! </p>
+    </div>
+    <div class="stackbox">
+      <p> Here's some more text! </p>
+    </div>
+    <div class="stackbox">
+      <p> Here's even more text! </p>
+    </div>
+  </div>
