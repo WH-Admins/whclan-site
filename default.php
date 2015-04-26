b8bc7e60cc79e6e2385aeb8756768626ac2469aa
Magnetic Duck <zenmags@gmail.com>
2015-04-26 10:28:20 +0000

    starting refactoring

--- default.php
+++ default.php
@@ -13,35 +13,33 @@
     </div>
   <div class="container headmargin">
     <div class="paddedbox">
-      <p> WH members, put this site on your favourites, make it your homepage, print it out and tape it to your forehead, or just refresh it every
-        once and a while; whatever makes more sense to you. <b>If you want to know what's going on</b>, you should be able to find the answer here. </p>
-      This site is the central element of the ongoing initiative to centralise and organise the WH clan; our goal is to make clan-wide communication easy, catalyze internal discussion in realtime and on Reddit, and finally to bring a more professional feel to the team as an organisation. These three goals can be summarized in the following three values:
-      <h3> communication, information, and organisation. </h3>
+      <p class="important"> WH members, put this site on your favourites, make it your homepage, print it out and tape it to your forehead, or just refresh it every
+        once and a while; whatever makes more sense to you. </p>
+      <p> We believe that it is our collective interest as a clan to improve ourselves as players and as a team. In this interest, we endeavour to communicate and organise ourselves more effectively. This site is at the core of this effort. 
     </div>
-    <div class="paddedbox nohead">
-      <p class="important nohead"> WH is dedicated to improving ourselves as players and as a competitive team. However, we also intend to play a role as a social agent, bringing progress and positivity to altitude through the organised effort of a player-base that is passionate about the game. </p>
+    <div>
+      <div class="split">
+        <div style="background-color:red"></div>
+      </div>
+      <div class="split">
+        <div style="background-color:blue"></div>
+      </div>
     </div>
     <div>
       <div class="split"><div class="paddedbox nohead">
-        <div class="titlebox">Our Agenda</div>
+        <div class="titlebox">We expect our members to:</div>
         <div class="contentbox">
-          <p> For developers </p>
           <ul>
-            <li> Add biography listing for our clan members in the communication page</li>
-            <li> Add a system to aid organisation of spontaneous practices from the IRC channel </li>
-          </ul>
-          <p> For the clan </p>
-          <ul>
-            <li> Now that the vast majority of our active members use reddit and IRC, organise some more team practices and start having more conversations on IRC </li>
-            <li> Finalize migration from the google group to reddit. </li>
-            <li> Welcome the members of the 'young' clan into our social circles! They want to learn altitude. Let's help them out. </li>
-            <li> Put on a good show in APL. </li>
+            <li> Use IRC and Reddit to communicate. </li>
+            <li> Use Mumble for in-game communication. </li>
+            <li> Think critically and make suggestions. </li>
+            <li> Follow instructions when necessary. </li>
           </ul>
         </div> <!-- content box ends -->
       </div> </div>
-      <div class="split"><div class="nobox">
+      <div class="split">
         <div align="center"> <img style="width: 100%;" src="http://i.imgur.com/XYDxm10.jpg" width=500/> </div>
-      </div></div>
+      </div>
     </div>
   </div>
 </body>
