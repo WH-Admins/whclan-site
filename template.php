cf406312f1cba5337eb73140dc2963e867a4660f
Magnetic Duck <zenmags@gmail.com>
2015-04-26 12:49:05 +0000

    simple fix for smaller screen widths

--- template.php
+++ template.php
@@ -10,15 +10,17 @@
     <h1 id="bannertext"> #headtext </h1>
     <div class="bannerimg" style="background-image:url(images/head-randa-left.png)"> </div>
   </div>
-  <div id="contentarea">
-    <div class="stackbox">
-      <p> Here's some text! </p>
-    </div>
-    <div class="stackbox">
-      <p> Here's some more text! </p>
-    </div>
-    <div class="stackbox">
-      <p> Here's even more text! </p>
+  <div id="contentholder">
+    <div id="contentarea">
+      <div class="stackbox">
+        <p> Here's some text! </p>
+      </div>
+      <div class="stackbox">
+        <p> Here's some more text! </p>
+      </div>
+      <div class="stackbox">
+        <p> Here's even more text! </p>
+      </div>
     </div>
   </div>
 </body>
