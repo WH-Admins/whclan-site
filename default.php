5e16ebf41fa90c138513e7d27107807aef7e6f47
Magnetic Duck <zenmags@gmail.com>
2015-04-26 13:40:42 +0000

    working on default.php

--- default.php
+++ default.php
@@ -6,34 +6,27 @@
 <body>
   <style> #org-link { font-weight:bold } </style>
   <?php include "header.php"; ?>
-    <div class="headbox">
-      <div id="headimg" style="background-image:url(images/head-randa-left.png)"> </div>
-      <h2 id="headtext">This is the central hub for all of WH's action.</h2>
-      <div id="headimg" style="background-image:url(images/head-loop-right.png)"> </div>
-    </div>
+  <div id="sitebanner">
+    <div class="bannerimg" style="background-image:url(images/head-randa-left.png)"> </div>
+    <h2 id="bannertext">This is the central hub for all of WH's action.</h2>
+    <div id="bannerimg" style="background-image:url(images/head-loop-right.png)"> </div>
+  </div>
   <div id="contentarea"
-    <div class="stackbox">
+    <div class="stackbox"> <!-- site description -->
       <p class="important"> WH members, put this site on your favourites, make it your homepage, print it out and tape it to your forehead, or just refresh it every
         once and a while; whatever makes more sense to you. </p>
       <p> We believe that it is our collective interest as a clan to improve ourselves as players and as a team. In this interest, we endeavour to communicate and organise ourselves more effectively. This site is at the core of this effort. 
     </div>
-    <div class="stackbox">
-      <style>
-        div.bigbutton {
-          background-color:red;
-          text-align:center;
-          height:20px;
-        }
-      </style>
-      <div class="split bigbutton">
-        Click here for a thing.
-      </div>
-      <div class="split bigbutton" style="background-color:blue">
-        CLick here for another things.
-      </div>
+    <div class="stackbox"> <!-- big links to reddit and IRC -->
+      <div class="split"> <div class="paddedbox">
+        Click here for a thing
+      </div> </div>
+      <div class="split"> <div class="paddedbox">
+        Click here for another things
+      </div> </div>
     </div>
-    <div class="stackbox">
-      <div class="split"><div class="paddedbox nohead">
+    <div class="stackbox"> <!-- expectations and clan pie -->
+      <div class="split"><div class="paddedbox nohead"> <!-- split 1 -->
         <div class="titlebox">We expect our members to:</div>
         <div class="contentbox">
           <ul>
@@ -42,11 +35,11 @@
             <li> Think critically and make suggestions. </li>
             <li> Follow instructions when necessary. </li>
           </ul>
-        </div> <!-- content box ends -->
-      </div> </div>
-      <div class="split">
+        </div> 
+      </div> </div> 
+      <div class="split"> <!-- split 2 -->
         <div align="center"> <img style="width: 100%;" src="http://i.imgur.com/XYDxm10.jpg" width=500/> </div>
-      </div>
+      </div> <!-- split 2 ends -->
     </div>
   </div>
 </body>
