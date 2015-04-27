f9df40c2027177629651a926c936aedf6cd3fce6
Magnetic Duck <zenmags@gmail.com>
2015-04-27 13:47:24 +0000

    video page validation

--- videos.php
+++ videos.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
-<html>
+<?php include "includes/doctype.php" ?>
 <head>
   <title>WH Videos</title>
   <?php include "includes/imports.php"; ?>
+  <style type="text/css"> #videos-link { font-weight:bold; } </style>
 </head>
 <body>
-  <style> #videos-link { font-weight:bold; } </style>
 
   <?php include "includes/header.php"; ?>
   <div id="sitebanner">
@@ -21,58 +20,56 @@
    
     <div class="stackbox">
 
-<h3> April 19th, APL Week 1 </h3>
-<div class="thumbgroup">
-<div class="videothumb"><a href="https://youtu.be/GdLxdeIslTo">
-<img src="thumbs/190415_rotw_qq_scrapyard.png"></a>
-<div class="thumbtext"> &lt8) RoTW vs qq - <b>Scrapyard</b> </div>
-</div>
-<div class="videothumb"><a href="https://youtu.be/y8vQoDcaAtc">
-<img src="thumbs/190415_rotw_qq_justice.png"></a>
-<div class="thumbtext"> &lt8) RoTW vs qq - <b>Justice</b> </div>
-</div>
+      <h3> April 19th, APL Week 1 </h3>
+      <div class="thumbgroup">
+      <div class="videothumb"><a href="https://youtu.be/GdLxdeIslTo">
+      <img alt="video thumbnail" src="thumbs/190415_rotw_qq_scrapyard.png"></a>
+      <div class="thumbtext"> &lt8) RoTW vs qq - <b>Scrapyard</b> </div>
+      </div>
+      <div class="videothumb"><a href="https://youtu.be/y8vQoDcaAtc">
+      <img alt="video thumbnail" src="thumbs/190415_rotw_qq_justice.png"></a>
+      <div class="thumbtext"> &lt8) RoTW vs qq - <b>Justice</b> </div>
+      </div>
 
-<div class="videothumb"><a href="https://youtu.be/bOtnMlbzP98">
-<img src="thumbs/190415_eu_i_cave.png"></a>
-<div class="thumbtext"> &lt8) EU vs &lti&gt- <b>Cave</b> </div>
-</div>
-<div class="videothumb"><a href="https://youtu.be/MdOcRxDc_VQ">
-<img src="thumbs/190415_eu_i_focus.png"></a>
-<div class="thumbtext"> &lt8) EU vs &lti&gt - <b>Focus</b> </div>
-</div>
+      <div class="videothumb"><a href="https://youtu.be/bOtnMlbzP98">
+      <img alt="video thumbnail" src="thumbs/190415_eu_i_cave.png"></a>
+      <div class="thumbtext"> &lt8) EU vs &lti&gt- <b>Cave</b> </div>
+      </div>
+      <div class="videothumb"><a href="https://youtu.be/MdOcRxDc_VQ">
+      <img alt="video thumbnail" src="thumbs/190415_eu_i_focus.png"></a>
+      <div class="thumbtext"> &lt8) EU vs &lti&gt - <b>Focus</b> </div>
+      </div>
 
-<div class="videothumb"><a href="https://youtu.be/is2MyPVBMAI">
-<img src="thumbs/190415_eu_qq_asteroids.png"></a>
-<div class="thumbtext"> &lt8) EU vs qq - <b>Asteroids</b> </div>
-</div>
-<div class="videothumb"><a href="https://youtu.be/iqy4-vDVZ_k">
-<img src="thumbs/190415_eu_qq_core.png"></a>
-<div class="thumbtext"> &lt8) EU vs qq - <b>Core</b> </div>
-</div>
-</div>
+      <div class="videothumb"><a href="https://youtu.be/is2MyPVBMAI">
+      <img alt="video thumbnail" src="thumbs/190415_eu_qq_asteroids.png"></a>
+      <div class="thumbtext"> &lt8) EU vs qq - <b>Asteroids</b> </div>
+      </div>
+      <div class="videothumb"><a href="https://youtu.be/iqy4-vDVZ_k">
+      <img alt="video thumbnail" src="thumbs/190415_eu_qq_core.png"></a>
+      <div class="thumbtext"> &lt8) EU vs qq - <b>Core</b> </div>
+      </div>
+      </div>
 
-<h3> March 24th, Scrim vs Implosion </h3>
-<div class="thumbgroup">
-<div class="videothumb"><a href="https://youtu.be/Jt15OAAyywE">
-<img src="thumbs/240315_cave.png"></a>
-<div class="thumbtext"> &lt8) vs &lti&gt - <b>Cave</b> </div>
-</div>
-<div class="videothumb"><a href="https://youtu.be/zggwVl-8qOQ">
-<img src="thumbs/240315_asteroids.png"></a>
-<div class="thumbtext"> &lt8) vs &lti&gt - <b>Asteroids</b> </div>
-</div>
-<div class="videothumb"><a href="https://youtu.be/pvfJCZk150w">
-<img src="thumbs/240315_focus.png"></a>
-<div class="thumbtext"> &lt8) vs &lti&gt - <b>Focus</b> </div>
-</div>
-<div class="videothumb"><a href="https://youtu.be/1D0fF4BX8-A">
-<img src="thumbs/240315_grotto.png"></a>
-<div class="thumbtext"> &lt8) vs &lti&gt - <b>Grotto</b> </div>
-</div>
-</div>
+      <h3> March 24th, Scrim vs Implosion </h3>
+      <div class="thumbgroup">
+      <div class="videothumb"><a href="https://youtu.be/Jt15OAAyywE">
+      <img alt="video thumbnail" src="thumbs/240315_cave.png"></a>
+      <div class="thumbtext"> &lt8) vs &lti&gt - <b>Cave</b> </div>
+      </div>
+      <div class="videothumb"><a href="https://youtu.be/zggwVl-8qOQ">
+      <img alt="video thumbnail" src="thumbs/240315_asteroids.png"></a>
+      <div class="thumbtext"> &lt8) vs &lti&gt - <b>Asteroids</b> </div>
+      </div>
+      <div class="videothumb"><a href="https://youtu.be/pvfJCZk150w">
+      <img alt="video thumbnail" src="thumbs/240315_focus.png"></a>
+      <div class="thumbtext"> &lt8) vs &lti&gt - <b>Focus</b> </div>
+      </div>
+      <div class="videothumb"><a href="https://youtu.be/1D0fF4BX8-A">
+      <img alt="video thumbnail" src="thumbs/240315_grotto.png"></a>
+      <div class="thumbtext"> &lt8) vs &lti&gt - <b>Grotto</b> </div>
+      </div> </div>
 
-</div>
-
-</div>
+    </div> <!-- end stackbox -->
+  </div> <!-- end contentarea -->
 </body>
 </html>
