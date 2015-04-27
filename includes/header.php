f6ba7abd832fdeef12003eba9dd7ecef5cf243ff
Magnetic Duck <zenmags@gmail.com>
2015-04-27 19:19:37 +0000

    removed logos, but they will be back

--- includes/header.php
+++ includes/header.php
@@ -10,25 +10,20 @@
 <div id="nav"> <div id="navcontent">
   <span id="navtitle"> Website of the <b>Wreaking Havoc</b> Clan </span>
   <div id="navlinks">
-    <span class="logo">&lt;8)</span>
     <span id="org-link"> 
       <a href="default.php" class="navbtn">home</a></span>
 
-    <span class="logo">&lt;8)</span>
     <span id="info-link"> 
       <a href="info.php" class="navbtn">info</a></span>
 
-    <span class="logo">&lt;8)</span>
     <span id="videos-link"> 
       <a href="videos.php" class="navbtn">videos</a></span>
 <?php
     if (isset($_SESSION['user_name'])) {
 ?>
-    <span class="logo">&lt;8)</span>
     <span id="restricted-link">
       <a href="restricted.php" class="navbtn">clan-only</a></span>
 
-    <span class="logo" style="padding-right:20px;"> | </span>
 
     <span id="logout-link">
       <a href="login.php" class="navbtn">logout</a></span>
@@ -45,7 +40,6 @@
       }
     } else {
 ?>
-    <span class="logo" style="padding-left:20px; padding-right:20px;"> | </span>
 
     <span id="login-link">
       <a href="login.php" class="navbtn">login</a></span>
