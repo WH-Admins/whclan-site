6852b5dd98e382cc23060615d6e9ef1cb510a3d6
Magnetic Duck <zenmags@gmail.com>
2015-04-27 09:29:43 +0000

    fixed subreddit description

--- subreddit.php
+++ subreddit.php
@@ -13,11 +13,11 @@
   <div id="contentarea">
     <div class="stackbox">
       <p class="important"> Reddit is an easy way to have discussions in communities of people. It will be taking the place of google groups in our clan. </p>
-      <p> To use our subreddit effectively, log onto the channel whenever you're online in some way. Meet your teammates, ask questions, check out the new IRC bot I'm writing, or do whatever happens to become possible. </p>
+      <p> To use our subreddit effectively, make sure to check it every once and a while and don't shy away from making replies to posts or new topics about things you'd like to talk about. We're still getting the ball rolling, but it'll be great once we do. </p>
     </div>
     <div class="stackbox">
       <div class="split"> <div class="sidebox">
-        <p> If you don't already have an IRC account, this would be a great time to make one. Once you do, be sure to make a post in our roll call thread (on the top of the page) so we know who you are on reddit. </p>
+        <p> If you don't already have an reddit account, this would be a great time to make one. Once you do, be sure to make a post in our roll call thread (on the top of the page) so we know who you are on reddit. </p>
         <a class="button" href="http://www.reddit.com/r/whclan"> here's our cool subreddit </a>
       </div> </div>
       <div class="split">
